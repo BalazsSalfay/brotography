29967b20796579bf491135007c2f190c4da67b13
BalazsSalfay <balazs.salfay@gmail.com>
2020-02-08 18:22:01 +0100

    Updates

---
+++ precache-manifest.980496d9f542a437e63c3a98e0d46102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78b61982a991dcd076608da9cb24b65",
+    "url": "/brotography/index.html"
+  },
+  {
+    "revision": "c46eacdcb9afbf60345a",
+    "url": "/brotography/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e26ace2228b5aa044bbc",
+    "url": "/brotography/static/js/2.aaeb8880.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/brotography/static/js/2.aaeb8880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46eacdcb9afbf60345a",
+    "url": "/brotography/static/js/main.66f5b7ff.chunk.js"
+  },
+  {
+    "revision": "5eeb7b1bde916d03d323",
+    "url": "/brotography/static/js/runtime-main.6a1cec26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/brotography/static/media/logo.5d5d9eef.svg"
+  }
+]);
